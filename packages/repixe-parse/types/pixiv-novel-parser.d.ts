cdcff3095fc52e68046a657ec5a76b8fc380a2d8
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-03-05 21:53:03 +0900

    feat: add `pixiv-novel-parser` package type-def

---
+++ packages/repixe-parse/types/pixiv-novel-parser.d.ts
@@ -0,0 +1,64 @@
+// Type definition for npm module "pixiv-novel-parser".
+
+declare module "pixiv-novel-parser" {
+  export class Parser<T extends PixivNode> {
+    static parse<T extends PixivNode>(novel: string): T[];
+    tree: T[];
+    parse(novel: string): Parser<T>;
+  }
+
+  export type PixivNode = Text | PixivTag;
+  export type PixivTag =
+    | NewPage
+    | Ruby
+    | Chapter
+    | PixivImage
+    | JumpPage
+    | JumpUrl;
+
+  interface Node {
+    type: string;
+  }
+
+  interface Tag extends Node {
+    type: "tag";
+    name: string;
+  }
+
+  export interface Text extends Node {
+    type: "text";
+    val: string;
+  }
+
+  export interface NewPage extends Tag {
+    name: "newpage";
+  }
+
+  export interface Ruby extends Tag {
+    name: "rb";
+    rubyBase: string;
+    rubyText: string;
+  }
+
+  export interface Chapter extends Tag {
+    name: "chapter";
+    title: (Text | Ruby)[];
+  }
+
+  export interface PixivImage extends Tag {
+    name: "pixivimage";
+    illustID: string;
+    pageNumber?: number;
+  }
+
+  export interface JumpPage extends Tag {
+    name: "jump";
+    pageNumber: number;
+  }
+
+  export interface JumpUrl extends Tag {
+    name: "jumpuri";
+    title: (Text | Ruby)[];
+    uri: string;
+  }
+}

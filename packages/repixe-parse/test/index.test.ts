8659fd66f3ac26c46c8c38e3a95e99c66fc7ae73
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-03-06 20:20:27 +0900

    test[working]: rename values

--- packages/repixe-parse/test/index.test.ts
+++
@@ -1,1 +0,0 @@
-import { repixeParse } from "@/index";
--- packages/repixe-parse/test/lib/index.test.ts
+++ packages/repixe-parse/test/lib/index.test.ts
@@ -7,95 +7,95 @@ describe("FlowContent", () => {
   describe("Heading", () => {
     test("Ruby を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Text を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("PageHeading", () => {
     test("ちゃんとページを分割できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("ページ番号が連続で採番される", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("Paragraph", () => {
     test("Link を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Image を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Ruby を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Text を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("1個の改行は Break になる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("2個の改行は新たな Paragraph に分割される", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("前後の改行を取り除く", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
 });
@@ -103,55 +103,55 @@ describe("PhrasingContent", () => {
   describe("Link", () => {
     test("正常な URL をちゃんと Link にできる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Ruby を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("Text を格納できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("Image", () => {
     test("画像形式の PixivImage をちゃんと変換できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
     test("漫画形式の PixivImage をちゃんと変換できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("PageHeading", () => {
     test("ジャンプ先の PageHeading が必ず存在する", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
 });
@@ -159,31 +159,31 @@ describe("StaticPhrasingContent", () => {
   describe("Break", () => {
     test("1個の改行は Break になる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("Ruby", () => {
     test("ルビをちゃんと認識できる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
   describe("Text", () => {
     test("ただのテキストはちゃんとただのテキストになる", () => {
       const source = "";
-      const expectedAst: Root = {
+      const expected: Root = {
         type: "root",
         children: []
       };
-      expect(fromPixivNovel(source)).toEqual(expectedAst);
+      expect(fromPixivNovel(source)).toEqual(expected);
     });
   });
 });

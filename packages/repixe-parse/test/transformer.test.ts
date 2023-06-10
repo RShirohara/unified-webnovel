5ce67a0d80101ed5b4354df67f97c4d0b07fc1c1
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-06-10 20:38:46 +0900

    test: add transformer mixed tests

--- packages/repixe-parse/test/transformer.test.ts
+++ packages/repixe-parse/test/transformer.test.ts
@@ -19,6 +19,7 @@ describe("Text", () => {
     console.info("pixiv-novel-parser output", Parser.parse(source));
     expect(fromPixivNovel(source)).toEqual(expected);
   });
+
   test("連続した改行を1つだけ持つ場合は2つの段落に分割される", () => {
     const source = "一つ目の段落\n\n二つ目の段落";
     const expected: Root = {
@@ -37,6 +38,7 @@ describe("Text", () => {
     console.info("pixiv-novel-parser output", Parser.parse(source));
     expect(fromPixivNovel(source)).toEqual(expected);
   });
+
   test("連続した改行を2つ以上持つ場合は3つ以上の段落に分割される", () => {
     const source =
       "一つ目の段落\n\n二つ目の段落\n\r\n三つ目の段落\n\n\r\n\n\r\n四つ目の段落\n四つ目の段落の二つ目の行";
@@ -108,6 +110,63 @@ describe("Mixed", () => {
             { type: "text", value: "二つ目の" },
             { type: "ruby", value: "段落", ruby: "だんらく" },
           ],
+        },
+      ],
+    };
+    console.info("pixiv-novel-parser output", Parser.parse(source));
+    expect(fromPixivNovel(source)).toEqual(expected);
+  });
+
+  test("段落の中間に PhrasingContent", () => {
+    const source =
+      "一つ目の段落\n\n二つ目の[[rb:段落>だんらく]]の中の\n改行\n\n三つ目の段落";
+    const expected: Root = {
+      type: "root",
+      children: [
+        {
+          type: "paragraph",
+          children: [{ type: "text", value: "一つ目の段落" }],
+        },
+        {
+          type: "paragraph",
+          children: [
+            { type: "text", value: "二つ目の" },
+            { type: "ruby", value: "段落", ruby: "だんらく" },
+            { type: "text", value: "の中の" },
+            { type: "break" },
+            { type: "text", value: "改行" },
+          ],
+        },
+        {
+          type: "paragraph",
+          children: [{ type: "text", value: "三つ目の段落" }],
+        },
+      ],
+    };
+    console.info("pixiv-novel-parser output", Parser.parse(source));
+    expect(fromPixivNovel(source)).toEqual(expected);
+  });
+
+  test("段落の末尾に PhrasingContent", () => {
+    const source =
+      "一つ目の段落\n\n二つ目の[[rb:段落>だんらく]]\n\n三つ目の段落";
+    const expected: Root = {
+      type: "root",
+      children: [
+        {
+          type: "paragraph",
+          children: [{ type: "text", value: "一つ目の段落" }],
+        },
+        {
+          type: "paragraph",
+          children: [
+            { type: "text", value: "二つ目の" },
+            { type: "ruby", value: "段落", ruby: "だんらく" },
+          ],
+        },
+        {
+          type: "paragraph",
+          children: [{ type: "text", value: "三つ目の段落" }],
         },
       ],
     };

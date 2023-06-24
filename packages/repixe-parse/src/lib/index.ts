05a2c1de01a71ef60c11c6e0d5598d0edc85421e
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-06-25 00:20:57 +0900

    ci(test): revert pul-request check trigger

--- packages/repixe-parse/src/lib/index.ts
+++ packages/repixe-parse/src/lib/index.ts
@@ -6,5 +6,5 @@ import { transpile } from "./transpiler.js";
 
 export function fromPixivNovel(doc: string): Root {
   const pixivNodes = transform(Parser.parse(doc));
-  return { type: "root", children: transpile(pixivNodes), data: undefined };
+  return { type: "root", children: transpile(pixivNodes) };
 }

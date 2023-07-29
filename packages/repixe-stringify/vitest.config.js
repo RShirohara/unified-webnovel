88f88e88138251f6d839c92a32ec94cfd9cd560f
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-07-29 09:08:12 +0900

    test: import tsconfig to vitest config (#79)

--- packages/repixe-stringify/vitest.config.js
+++ packages/repixe-stringify/vitest.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   test: {
@@ -6,6 +7,7 @@ export default defineConfig({
     include: ["./test/**/*.test.ts"],
     environment: "node"
   },
+  plugins: [tsconfigPaths({ root: "./test" })],
   resolve: {
     alias: {
       "~": `./src`

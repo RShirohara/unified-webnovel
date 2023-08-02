2401a8c762a658b1f8cea0ff116e1aac23dbeeac
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-08-02 21:58:29 +0900

    test: use `proess()` and async-wait syntax

--- packages/repixe/test/unified.test.ts
+++ packages/repixe/test/unified.test.ts
@@ -1,11 +1,10 @@
 import { expect, test } from "vitest";
 
-import type { Root } from "@rshirohara/pxast";
 import { repixe } from "~/";
 
-test("Can Parse", () => {
+test("repixe", async () => {
   const source = [
-    "unified で Pixiv 小説構文をパースできるかのテスト。\n",
+    "unified で Pixiv 小説構文を処理できるかのテスト。\n",
     "ここが二[[rb:段落>だんらく]]目。",
     "ここが二行目。\n\n",
     "ここから三段落目。",
@@ -14,109 +13,8 @@ test("Can Parse", () => {
     "ここからページが変わる。",
     "[jump:01]ページへの参照。"
   ].join("\r\n");
-  const expected: Root = {
-    type: "root",
-    children: [
-      { type: "pageHeading", pageNumber: 1 },
-      {
-        type: "paragraph",
-        children: [
-          {
-            type: "text",
-            value: "unified で Pixiv 小説構文をパースできるかのテスト。"
-          }
-        ]
-      },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここが二" },
-          { type: "ruby", value: "段落", ruby: "だんらく" },
-          { type: "text", value: "目。" },
-          { type: "break" },
-          { type: "text", value: "ここが二行目。" }
-        ]
-      },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここから三段落目。" },
-          { type: "break" },
-          {
-            type: "link",
-            url: "https://example.com",
-            children: [{ type: "text", value: "リンク" }]
-          },
-          { type: "text", value: "も使える。" }
-        ]
-      },
-      { type: "pageHeading", pageNumber: 2 },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここからページが変わる。" },
-          { type: "break" },
-          { type: "pageReference", pageNumber: 1 },
-          { type: "text", value: "ページへの参照。" }
-        ]
-      }
-    ]
-  };
-
-  expect(repixe().parse(source)).toEqual(expected);
-});
-
-test("Can Stringify", () => {
-  const source: Root = {
-    type: "root",
-    children: [
-      { type: "pageHeading", pageNumber: 1 },
-      {
-        type: "paragraph",
-        children: [
-          {
-            type: "text",
-            value: "unified で Pixiv 小説構文をレンダリングできるかのテスト。"
-          }
-        ]
-      },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここが二" },
-          { type: "ruby", value: "段落", ruby: "だんらく" },
-          { type: "text", value: "目。" },
-          { type: "break" },
-          { type: "text", value: "ここが二行目。" }
-        ]
-      },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここから三段落目。" },
-          { type: "break" },
-          {
-            type: "link",
-            url: "https://example.com",
-            children: [{ type: "text", value: "リンク" }]
-          },
-          { type: "text", value: "も使える。" }
-        ]
-      },
-      { type: "pageHeading", pageNumber: 2 },
-      {
-        type: "paragraph",
-        children: [
-          { type: "text", value: "ここからページが変わる。" },
-          { type: "break" },
-          { type: "pageReference", pageNumber: 1 },
-          { type: "text", value: "ページへの参照。" }
-        ]
-      }
-    ]
-  };
   const expected = [
-    "unified で Pixiv 小説構文をレンダリングできるかのテスト。\n",
+    "unified で Pixiv 小説構文を処理できるかのテスト。\n",
     "ここが二[[rb: 段落 > だんらく]]目。",
     "ここが二行目。\n",
     "ここから三段落目。",
@@ -126,5 +24,5 @@ test("Can Stringify", () => {
     "[jump:1]ページへの参照。"
   ].join("\n");
 
-  expect(repixe().stringify(source)).toEqual(expected);
+  expect((await repixe().process(source)).toString()).toEqual(expected);
 });

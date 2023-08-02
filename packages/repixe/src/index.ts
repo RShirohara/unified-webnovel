7526549b38361911d3aa3713e2d9aeafcba70401
Ray Shirohara <47940764+RShirohara@users.noreply.github.com>
2023-08-02 22:01:59 +0900

    refactor: fix format and organize import

--- packages/repixe/src/index.ts
+++ packages/repixe/src/index.ts
@@ -1,6 +1,5 @@
-import { unified } from "unified";
-
 import { repixeParse } from "@rshirohara/repixe-parse";
 import { repixeStringify } from "@rshirohara/repixe-stringify";
+import { unified } from "unified";
 
 export const repixe = unified().use(repixeParse).use(repixeStringify).freeze();

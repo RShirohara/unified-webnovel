0155da5016827339e5206e2f71096c09e5c93571
Ray Shirohara <RShirohara@proton.me>
2024-03-01 21:31:23 +0900

    feat: add preprocessor

--- packages/rekurke-parse/src/lib/index.ts
+++ packages/rekurke-parse/src/lib/index.ts
@@ -1,6 +1,7 @@
 import type { Root } from "@rshirohara/kkast";
 import { parse } from "./parser.peg.js";
+import { preprocess } from "./preprocessor.js";
 
 export function fromKakuyomuNovel(doc: string): Root {
-  return parse(doc);
+  return parse(preprocess(doc));
 }

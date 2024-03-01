d029278b020341e32ba4aac5a513188148d59984
Ray Shirohara <RShirohara@proton.me>
2024-03-01 21:31:48 +0900

    feat: add postprocessor

--- packages/rekurke-parse/src/lib/index.ts
+++ packages/rekurke-parse/src/lib/index.ts
@@ -1,7 +1,8 @@
 import type { Root } from "@rshirohara/kkast";
 import { parse } from "./parser.peg.js";
+import { postprocess } from "./postprocessor.js";
 import { preprocess } from "./preprocessor.js";
 
 export function fromKakuyomuNovel(doc: string): Root {
-  return parse(preprocess(doc));
+  return postprocess(parse(preprocess(doc)));
 }

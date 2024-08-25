92196eb1e7316ed291d2542fa1879824dfee4648
Ray Shirohara <RShirohara@proton.me>
2024-08-25 22:58:13 +0900

    feat: add postprocessor

---
+++ packages/repixe-rekurke/src/lib/postprocessor.ts
@@ -0,0 +1,44 @@
+import type { KkastContent, Root } from "@rshirohara/kkast";
+import type { Options } from "./options.js";
+
+type KkastNode = KkastContent | Root;
+type Postprocessor<T extends KkastNode> = (node: T, option: Options) => T;
+
+export const postprocess: Postprocessor<Root> = (node, options) => {
+  const processors: Postprocessor<Root>[] = [
+    removeEmptyNode,
+    insertParagraphMargin,
+  ];
+  let result: Root = { ...node };
+  for (const processor of processors) {
+    result = processor(result, options);
+  }
+  return result;
+};
+
+function removeEmptyNode<T extends Root>(node: T, _: Options): T {
+  function removeNode<T extends KkastContent>(node: T): T | undefined {
+    if (node.type === "paragraph" && node.children.length === 0) {
+      return undefined;
+    }
+    return node;
+  }
+
+  return {
+    ...node,
+    children: [...node.children]
+      .map((node) => removeNode(node))
+      .filter((node) => node !== undefined),
+  };
+}
+
+function insertParagraphMargin<T extends Root>(node: T, options: Options): T {
+  return {
+    ...node,
+    children: [...node.children].flatMap((value, index) =>
+      index + 1 < node.children.length
+        ? [value, { type: "paragraphMargin", size: 1 }]
+        : [value],
+    ),
+  };
+}

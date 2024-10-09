86259ec6f9e081e2badcfdc4f16ab5f203aa0881
Ray Shirohara <RShirohara@proton.me>
2024-10-09 21:43:39 +0900

    test: add position info to `PhrasingContent/Break`

--- packages/repixe-parse/test/pxast/phrasingContent/break.test.ts
+++ packages/repixe-parse/test/pxast/phrasingContent/break.test.ts
@@ -12,12 +12,40 @@ describe("内部コンテンツ", () => {
           {
             type: "paragraph",
             children: [
-              { type: "text", value: "一行目" },
-              { type: "break" },
-              { type: "text", value: "二行目" },
+              {
+                type: "text",
+                value: "一行目",
+                position: {
+                  start: { line: 1, column: 1, offset: 0 },
+                  end: { line: 1, column: 4, offset: 3 },
+                },
+              },
+              {
+                type: "break",
+                position: {
+                  start: { line: 1, column: 4, offset: 3 },
+                  end: { line: 2, column: 1, offset: 4 },
+                },
+              },
+              {
+                type: "text",
+                value: "二行目",
+                position: {
+                  start: { line: 2, column: 1, offset: 4 },
+                  end: { line: 2, column: 4, offset: 7 },
+                },
+              },
             ],
+            position: {
+              start: { line: 1, column: 1, offset: 0 },
+              end: { line: 2, column: 4, offset: 7 },
+            },
           },
         ],
+        position: {
+          start: { line: 1, column: 1, offset: 0 },
+          end: { line: 2, column: 4, offset: 7 },
+        },
       };
       expect(fromPixivNovel(source)).toEqual(expected);
     });
@@ -30,12 +58,40 @@ describe("内部コンテンツ", () => {
           {
             type: "paragraph",
             children: [
-              { type: "text", value: "一行目" },
-              { type: "break" },
-              { type: "text", value: "二行目" },
+              {
+                type: "text",
+                value: "一行目",
+                position: {
+                  start: { line: 1, column: 1, offset: 0 },
+                  end: { line: 1, column: 4, offset: 3 },
+                },
+              },
+              {
+                type: "break",
+                position: {
+                  start: { line: 1, column: 4, offset: 3 },
+                  end: { line: 2, column: 1, offset: 4 },
+                },
+              },
+              {
+                type: "text",
+                value: "二行目",
+                position: {
+                  start: { line: 2, column: 1, offset: 4 },
+                  end: { line: 2, column: 4, offset: 7 },
+                },
+              },
             ],
+            position: {
+              start: { line: 1, column: 1, offset: 0 },
+              end: { line: 2, column: 4, offset: 7 },
+            },
           },
         ],
+        position: {
+          start: { line: 1, column: 1, offset: 0 },
+          end: { line: 2, column: 4, offset: 7 },
+        },
       };
       expect(fromPixivNovel(source)).toEqual(expected);
     });
@@ -48,12 +104,40 @@ describe("内部コンテンツ", () => {
           {
             type: "paragraph",
             children: [
-              { type: "text", value: "一行目" },
-              { type: "break" },
-              { type: "text", value: "二行目" },
+              {
+                type: "text",
+                value: "一行目",
+                position: {
+                  start: { line: 1, column: 1, offset: 0 },
+                  end: { line: 1, column: 4, offset: 3 },
+                },
+              },
+              {
+                type: "break",
+                position: {
+                  start: { line: 1, column: 4, offset: 3 },
+                  end: { line: 2, column: 1, offset: 5 },
+                },
+              },
+              {
+                type: "text",
+                value: "二行目",
+                position: {
+                  start: { line: 2, column: 1, offset: 5 },
+                  end: { line: 2, column: 4, offset: 8 },
+                },
+              },
             ],
+            position: {
+              start: { line: 1, column: 1, offset: 0 },
+              end: { line: 2, column: 4, offset: 8 },
+            },
           },
         ],
+        position: {
+          start: { line: 1, column: 1, offset: 0 },
+          end: { line: 2, column: 4, offset: 8 },
+        },
       };
       expect(fromPixivNovel(source)).toEqual(expected);
     });

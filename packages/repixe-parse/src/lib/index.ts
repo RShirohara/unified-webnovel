89f4364db0138a85d34fbc893ee67f71c08acd5b
Ray Shirohara <RShirohara@proton.me>
2024-10-09 21:35:04 +0900

    feat: add postprocessor

--- packages/repixe-parse/src/lib/index.ts
+++ packages/repixe-parse/src/lib/index.ts
@@ -1,7 +1,8 @@
 import type { Root } from "@rshirohara/pxast";
 import { parse } from "./parser.peg.js";
+import { postprocess } from "./postprocessor.js";
 import { preprocess } from "./preprocessor.js";
 
 export function fromPixivNovel(doc: string): Root {
-  return parse(preprocess(doc));
+  return postprocess(parse(preprocess(doc)));
 }

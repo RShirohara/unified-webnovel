6e1e37a31a51d6b72c5e79adcd7415ce5e3e00a6
Ray Shirohara <RShirohara@proton.me>
2024-10-26 17:24:05 +0900

    refactor: AST convert process for repixe (#347)
    - Refactor ast convertion process for repixe.
    - Add merge process for options and defaultOptions.
    - Fix typo in README.

--- packages/repixe-rekurke/src/lib/options.ts
+++ packages/repixe-rekurke/src/lib/options.ts
@@ -1,7 +1,18 @@
 export interface Options {
+  preserveUnmatchedSyntax?: boolean;
+}
+
+interface InternalOptions {
   preserveUnmatchedSyntax: boolean;
 }
 
-export const defaultOptions: Options = {
+export const defaultOptions: InternalOptions = {
   preserveUnmatchedSyntax: false,
 };
+
+export function buildOptions(options?: Options | null): InternalOptions {
+  return {
+    ...defaultOptions,
+    ...options,
+  };
+}

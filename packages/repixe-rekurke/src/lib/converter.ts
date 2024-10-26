8a06164a73f8bb2fae410034edb381e5245196bc
Ray Shirohara <RShirohara@proton.me>
2024-10-26 17:07:51 +0900

    refactor: AST convert process for repixe

--- packages/repixe-rekurke/src/lib/converter.ts
+++ packages/repixe-rekurke/src/lib/converter.ts
@@ -1,18 +1,25 @@
 import type {
   Break as KkastBreak,
+  KkastContent,
+  FlowContent as KkastFlowContent,
   Paragraph as KkastParagraph,
+  PhrasingContent as KkastPhrasingContent,
   Root as KkastRoot,
   Ruby as KkastRuby,
   Text as KkastText,
 } from "@rshirohara/kkast";
 import type {
   Break as PxastBreak,
+  PxastContent,
+  FlowContent as PxastFlowContent,
   Heading as PxastHeading,
   Image as PxastImage,
+  InlinePhrasingContent as PxastInlinePhrasingContent,
   Link as PxastLink,
   PageBreak as PxastPageBreak,
   PageReference as PxastPageReference,
   Paragraph as PxastParagraph,
+  PhrasingContent as PxastPhrasingContent,
   Root as PxastRoot,
   Ruby as PxastRuby,
   Text as PxastText,
@@ -23,55 +30,70 @@ export function convertRoot(tree: PxastRoot, options: Options): KkastRoot {
   return {
     type: "root",
     children: [...tree.children]
-      .flatMap((node) => {
-        switch (node.type) {
-          case "heading": {
-            return convertHeading(node, options);
-          }
-          case "pageBreak": {
-            return convertPageBreak(node, options);
-          }
-          case "paragraph": {
-            return convertParagraph(node, options);
-          }
-          case "break": {
-            return convertBreak(node, options);
-          }
-          case "image": {
-            return convertImage(node, options);
-          }
-          case "link": {
-            return convertLink(node, options);
-          }
-          case "pageReference": {
-            return convertPageReference(node, options);
-          }
-          case "ruby": {
-            return convertRuby(node, options);
-          }
-          case "text": {
-            return convertText(node, options);
-          }
-        }
-      })
+      .flatMap((node) => convertRootChildren(node, options))
       .filter((node) => node !== undefined),
   };
 }
 
+export function convertRootChildren(
+  node: PxastContent,
+  options: Options,
+): (KkastContent | undefined)[] {
+  return [node].flatMap((node): (KkastContent | undefined)[] | undefined => {
+    switch (node.type) {
+      case "heading":
+      case "pageBreak":
+      case "paragraph": {
+        return convertFlowContent(node, options).filter(
+          (node) => node !== undefined,
+        );
+      }
+      case "break":
+      case "image":
+      case "link":
+      case "pageReference":
+      case "ruby":
+      case "text": {
+        return convertPhrasingContent(node, options).filter(
+          (node) => node !== undefined,
+        );
+      }
+      default: {
+        return undefined;
+      }
+    }
+  });
+}
+
 // FlowContent
+export function convertFlowContent(
+  node: PxastFlowContent,
+  options: Options,
+): (KkastFlowContent | undefined)[] {
+  return [node].flatMap((node) => {
+    switch (node.type) {
+      case "heading": {
+        return convertHeading(node, options);
+      }
+      case "pageBreak": {
+        return convertPageBreak(node, options);
+      }
+      case "paragraph": {
+        return convertParagraph(node, options);
+      }
+      default: {
+        return undefined;
+      }
+    }
+  });
+}
+
 function convertHeading(node: PxastHeading, options: Options): KkastParagraph {
   return {
     type: "paragraph",
-    children: [...node.children].map((node) => {
-      switch (node.type) {
-        case "ruby": {
-          return convertRuby(node, options);
-        }
-        case "text": {
-          return convertText(node, options);
-        }
-      }
-    }),
+    children: [...node.children]
+      .flatMap((node) => convertInlinePhrasingContent(node, options))
+      .filter((node) => node !== undefined),
   };
 }
 
@@ -95,33 +117,50 @@ function convertParagraph(
   return {
     type: "paragraph",
     children: [...node.children]
-      .flatMap((node) => {
-        switch (node.type) {
-          case "break": {
-            return convertBreak(node, options);
-          }
-          case "image": {
-            return convertImage(node, options);
-          }
-          case "link": {
-            return convertLink(node, options);
-          }
-          case "pageReference": {
-            return convertPageReference(node, options);
-          }
-          case "ruby": {
-            return convertRuby(node, options);
-          }
-          case "text": {
-            return convertText(node, options);
-          }
-        }
-      })
+      .flatMap((node) => convertPhrasingContent(node, options))
       .filter((node) => node !== undefined),
   };
 }
 
 // PhrasingContent
+export function convertPhrasingContent(
+  node: PxastPhrasingContent,
+  options: Options,
+): (KkastPhrasingContent | undefined)[] {
+  return [node].flatMap(
+    (
+      node,
+    ):
+      | KkastPhrasingContent
+      | undefined
+      | (KkastPhrasingContent | undefined)[] => {
+      switch (node.type) {
+        case "break": {
+          return convertBreak(node, options);
+        }
+        case "image": {
+          return convertImage(node, options);
+        }
+        case "link": {
+          return convertLink(node, options);
+        }
+        case "pageReference": {
+          return convertPageReference(node, options);
+        }
+        case "ruby":
+        case "text": {
+          return convertInlinePhrasingContent(node, options).filter(
+            (node) => node !== undefined,
+          );
+        }
+        default: {
+          return undefined;
+        }
+      }
+    },
+  );
+}
+
 function convertBreak(_node: PxastBreak, _options: Options): KkastBreak {
   return { type: "break" };
 }
@@ -133,9 +172,7 @@ function convertImage(
   if (options.preserveUnmatchedSyntax) {
     return {
       type: "text",
-      value: `[pixivimage:${node.illustId}${
-        node.pageNumber !== undefined ? `-${node.pageNumber.toString()}` : ""
-      }]`,
+      value: `[pixivimage:${node.illustId}${node.pageNumber !== undefined ? `-${node.pageNumber}` : ""}]`,
     };
   }
   return undefined;
@@ -155,21 +192,17 @@ function convertLink(
           case "text": {
             return node.value;
           }
+          default: {
+            return "";
+          }
         }
       })
       .join("");
     return [{ type: "text", value: `[[jumpuri: ${text} > ${node.url}]]` }];
   }
-  return [...node.children].map((node) => {
-    switch (node.type) {
-      case "ruby": {
-        return convertRuby(node, options);
-      }
-      case "text": {
-        return convertText(node, options);
-      }
-    }
-  });
+  return [...node.children]
+    .flatMap((node) => convertInlinePhrasingContent(node, options))
+    .filter((node) => node !== undefined);
 }
 
 function convertPageReference(
@@ -180,6 +213,28 @@ function convertPageReference(
     return { type: "text", value: `[jump:${node.pageNumber}]` };
   }
   return undefined;
+}
+
+// InlinePhrasingContent
+type KkastInlinePhrasingContent = KkastRuby | KkastText;
+
+export function convertInlinePhrasingContent(
+  node: PxastInlinePhrasingContent,
+  options: Options,
+): (KkastInlinePhrasingContent | undefined)[] {
+  return [node].flatMap((node) => {
+    switch (node.type) {
+      case "ruby": {
+        return convertRuby(node, options);
+      }
+      case "text": {
+        return convertText(node, options);
+      }
+      default: {
+        return undefined;
+      }
+    }
+  });
 }
 
 function convertRuby(node: PxastRuby, _: Options): KkastRuby {

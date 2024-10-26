99855083f85573267c6445f1c88b77a72cf8b4f1
Ray Shirohara <RShirohara@proton.me>
2024-10-26 17:17:05 +0900

    refactor: add merge process for options and defaultOptions

--- packages/repixe-rekurke/src/lib/index.ts
+++ packages/repixe-rekurke/src/lib/index.ts
@@ -1,11 +1,10 @@
 import type { Root as KkastRoot } from "@rshirohara/kkast";
 import type { Root as PxastRoot } from "@rshirohara/pxast";
 import { convertRoot } from "./converter.js";
-import { type Options, defaultOptions } from "./options.js";
+import { type Options, buildOptions } from "./options.js";
 import { postprocess } from "./postprocessor.js";
 
 export function toKkast(tree: PxastRoot, options?: Options | null): KkastRoot {
-  const option =
-    options !== undefined && options !== null ? options : defaultOptions;
+  const option = buildOptions(options);
   return postprocess(convertRoot(tree, option), option);
 }

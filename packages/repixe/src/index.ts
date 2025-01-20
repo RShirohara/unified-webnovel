eaf0e49a73e4b5a8396a6f814e9a3e10bb75b0c4
Ray Shirohara <RShirohara@proton.me>
2025-01-20 21:26:41 +0900

    fix: remove package `repixe`

--- packages/repixe/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { repixeParse } from "@rshirohara/repixe-parse";
-import { repixeStringify } from "@rshirohara/repixe-stringify";
-import { unified } from "unified";
-
-export const repixe = unified().use(repixeParse).use(repixeStringify).freeze();

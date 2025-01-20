9a2927abf35b4c3e4ba4be05e7eed84dca4c1d37
Ray Shirohara <RShirohara@proton.me>
2025-01-20 21:26:07 +0900

    fix: remove package `rekurke`

--- packages/rekurke/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { rekurkeParse } from "@rshirohara/rekurke-parse";
-import { rekurkeStringify } from "@rshirohara/rekurke-stringify";
-import { unified } from "unified";
-
-export const rekurke = unified()
-	.use(rekurkeParse)
-	.use(rekurkeStringify)
-	.freeze();
